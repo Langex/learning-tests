27c45722e2cedf3748d49ca2c63ea3e3849042b0
onhan <artem.shmahun@gmail.com>
2018-08-28 12:36:06 +0300

    #3: to the values of names

--- test_employees.py
+++ test_employees.py
@@ -8,3 +8,7 @@ DEFAULT_HEADER = 'application/json'
 SUCCESS = 200
 INCORRECT_HEADER = 400
 ADDED = 201
+
+FIRST_NAME = ('Frodo', 'Bilbo', 'Gandalf', 'Samwise', 'Meriadoc', 'Peregrin')
+LAST_NAME = ('Baggins', 'Baggins', 'the Grey', 'Gamgee', 'Brandybuck', 'Took')
+DESCRIPTION = ('ring bearer', 'burglar', 'wizard', 'gardener', 'pony rider', 'pipe smoker')

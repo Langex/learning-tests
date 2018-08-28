d6ea0d7aa82dda11cef6d6d96a257e02b3962054
onhan <artem.shmahun@gmail.com>
2018-08-28 13:10:11 +0300

    #3: staff inspection instruction https://i.imgur.com/YdSYAhT.png

--- test_employees.py
+++ test_employees.py
@@ -14,3 +14,12 @@ LAST_NAME = ('Baggins', 'Baggins', 'the Grey', 'Gamgee', 'Brandybuck', 'Took')
 DESCRIPTION = ('ring bearer', 'burglar', 'wizard', 'gardener', 'pony rider', 'pipe smoker')
 
 class TestEmployees(unittest.TestCase):
+
+	def __init__(self, *a, **kw):
+		super(TestEmployees, self).__init__(*a, **kw)
+		self.host = 'localhost:8080'
+		self.command = 'employees'
+		self.url = 'http://{}/api/{}'.format(self.host, self.command)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
